87c6e9ae005c083f3ecbd8040d6ba5ef5c103871
Momchil Anachkov <Momchil Anachkov>
2016-04-21 22:09:36 +0300

    Implement settings view

---
+++ TaskManager.Web.App/src/settings/controllers/SettingsEditCtrl.js
@@ -0,0 +1,29 @@
+﻿(function (angular) {
+  'use strict';
+
+  function SettingsEditCtrl(
+      $scope,
+      $state
+    ) {
+    $scope.editMode = false;
+
+    //TODO: Get the settings from the server
+    $scope.settings = {};
+
+    $scope.save = function () {
+      //TODO: Actually save the settings
+      $scope.editMode = false;
+    };
+
+    $scope.back = function () {
+      console.log('dafuq man?');
+      return $state.go('tasks', {});
+    };
+  }
+
+  SettingsEditCtrl.$inject = [
+      '$scope',
+      '$state'
+    ];
+  angular.module('taskManager').controller('SettingsEditCtrl', SettingsEditCtrl);
+}(angular));
